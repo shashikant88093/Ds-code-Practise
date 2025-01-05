415542fd469e3d703f586e2ea402f811be767ef2
shashikant kumar <shashi94goswami@gmail.com>
2025-01-05 21:52:17 +0530

    learning python

---
+++ Dsa-Python/Basic-Python/main.py
@@ -0,0 +1,46 @@
+myvariable = 2
+
+print(myvariable,type(myvariable))
+
+myfloat = 3.6
+print(myfloat,type(myfloat))
+
+mycomplex = 3 + 4j
+
+print(mycomplex,type(mycomplex))
+
+
+# STR single quotes
+
+name = 'John'
+
+print(name,type(name))
+
+namedouble = "John"  # double quotes
+
+print(namedouble,type(namedouble))
+
+nametriple = '''
+Shashikant 
+went 
+for hard work
+'''
+print(nametriple,type(nametriple))
+
+
+
+#  list(Array)
+
+name = [1,2,5,"shashikant"]
+print(name,type(name))
+
+
+#  input () function
+
+name = input()
+
+print(name,type(name)) # now name is a string
+
+nametype = int(input())
+
+print(nametype,type(nametype))
