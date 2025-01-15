1bdf54f0e02c05eef2f8a62f08806fe60b406708
shashikant kumar <shashi94goswami@gmail.com>
2025-01-15 17:12:28 +0530

    added the set function

---
+++ Dsa-Python/Basic-Python/set.py
@@ -0,0 +1,29 @@
+mySet = {2,3,4,5}
+
+
+print(mySet)
+
+mySet.add("shashikant")
+
+print(mySet)
+
+mySet2 = {7,9,8}
+
+mySet.update(mySet2)
+
+print(mySet)
+
+# remove , discard
+mySet.remove(2)
+print(mySet)
+
+mySet.discard(7)
+print(mySet)
+
+mySet.pop()
+print(mySet)
+
+# clear all by clear() method
+
+mySet.clear()
+print(mySet)
